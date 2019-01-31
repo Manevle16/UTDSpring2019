8d3e5ee2b44625fa9686583b0a2915845ebbe97c
Matthew <Madmatt505@gmail.com>
2019-01-31 16:30:36 -0600

    Changed name

--- MachineLearning/HW1DueFeb3/8_dplyr_exercise(1).R
+++ MachineLearning/HW1DueFeb3/8_dplyr_exercise(1).R
@@ -1,13 +1,19 @@
 library(dplyr)
 head(mtcars)
-
+df <- data.frame(mtcars)
 # 1. Return rows of cars that have an mpg value greater than 20 and 6 cylinders. 
+filter(df, mpg > 20 & cyl > 6) 
 # 2. Reorder the Data Frame by cyl first, then by descending wt. 
+arrange(df, cyl, desc(wt))
 # 3. Select the columns mpg and hp
+df[, c("mpg", "hp")]
 # 4. Select the distinct values of the gear column. 
+distinct(df["gear"])
 # 5. Create a new column called "Performance" which is calculated by hp 
 #    divided by wt. 
+mutate(df, Performance = hp/wt)
 # 6. Find the mean mpg value using dplyr. 
+
 # 7. Use pipe operators to get the mean hp value for cars with 6 cylinders. 
 
 
